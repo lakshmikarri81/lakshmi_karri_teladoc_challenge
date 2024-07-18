d32bccd4b405d4ea91973a32f2017d2ca6a94af5
lakshmikarri81 <lakshmikarri81@gmail.com>
2024-07-18 06:44:22 -0700

    Remove .idea/ directory and update .gitignore

--- src/main/java/pages/Locators.java
+++ src/main/java/pages/Locators.java
@@ -3,16 +3,16 @@ package pages;
 import org.openqa.selenium.By;
 
 public class Locators {
-    public static final By ADD_USER_BUTTON = By.xpath("//thead/tr[2]/td[1]/button[1]");
+    public static final By ADD_USER_BUTTON = By.xpath("//button[@class='btn btn-link pull-right' and @type='add']");
     public static final By USER_TABLE_BODY = By.xpath(".//tbody");
-    public static final By FIRST_NAME_FIELD = By.xpath("//tbody/tr[1]/td[2]/input[1]");
-    public static final By LAST_NAME_FIELD = By.xpath("//tbody/tr[2]/td[2]/input[1]");
-    public static final By USERNAME_FIELD = By.xpath("//tbody/tr[3]/td[2]/input[1]");
-    public static final By PASSWORD_FIELD = By.xpath("//tbody/tr[4]/td[2]/input[1]");
-    public static final By COMPANY_A = By.xpath("//tbody/tr[5]/td[2]/label[1]");
-    public static final By ROLE_DROPDOWN = By.xpath("//tbody/tr[6]/td[2]/select[1]");
-    public static final By EMAIL_FIELD = By.xpath("//tbody/tr[7]/td[2]/input[1]");
-    public static final By PHONE_FIELD = By.xpath("//tbody/tr[8]/td[2]/input[1]");
+    public static final By FIRST_NAME_FIELD = By.xpath("//input[@name='FirstName']");
+    public static final By LAST_NAME_FIELD = By.xpath("//input[@name='LastName']");
+    public static final By USERNAME_FIELD = By.xpath("//input[@name='UserName']");
+    public static final By PASSWORD_FIELD = By.xpath("//input[@name='Password']");
+    public static final By COMPANY_A = By.xpath("//label[input[@type='radio' and @name='optionsRadios' and @value='15']]");
+    public static final By ROLE_DROPDOWN = By.xpath("//select[@name='RoleId' and @ng-model='dataRow[column.map]']");
+    public static final By EMAIL_FIELD = By.xpath("//input[@name='Email']");
+    public static final By PHONE_FIELD = By.xpath("//input[@name='Mobilephone']");
     public static final By SAVE_BUTTON = By.xpath("//button[contains(text(),'Save')]");
     public static final By DELETE_BUTTON = By.xpath(".//button[@class='btn btn-link' and @ng-click='delUser()']");
     public static final By MODAL_DIALOG = By.xpath("//div[contains(@class, 'modal')]");
